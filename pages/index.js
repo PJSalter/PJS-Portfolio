a8a56f45ba09a12e154baa98517a57630a4f8f70
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-10-20 22:56:15 +0100

    styling portfolio title with rainbow effect

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to PJS-Coder-Portfolio</h1>
+        <RainbowTitle className={styles.title}>
+          Welcome to PJS-Coder-Portfolio
+        </RainbowTitle>
 
         <p className={styles.description}>
           Get started by editing{" "}
@@ -65,3 +67,20 @@ export default function Home() {
     </div>
   );
 }
+
+const RainbowTitle = styled.h1`
+  background: linear-gradient(
+    to right,
+    #ef5350,
+    #f48fb1,
+    #7e57c2,
+    #2196f3,
+    #26c6da,
+    #43a047,
+    #eeff41,
+    #f9a825,
+    #ff5722
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
