f14076cffc515fc033468b30b2abbcb48043ccda
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-10-25 23:02:43 +0100

    created styled components combined with tailwind styling as twin macro compatible

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,27 @@
-import react from "react";
+import React from "react";
+import Image from "next/image";
+import pic from "../public/assets/pjs-software-developer.png";
+import styled from "styled-components";
+import tw from "twin.macro";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <StyledDivForNavbar>
+      <NavContainer>
+        <div css={tw`w-16 h-16 mb-1 object-left-top`}>
+          <Image src={pic} alt="pjs-logo" />
+        </div>
+      </NavContainer>
+    </StyledDivForNavbar>
+  );
 };
 
 export default Navbar;
+
+const StyledDivForNavbar = styled.div`
+  ${tw`fixed w-full h-20 shadow-xl z-[100]`}
+`;
+
+const NavContainer = styled.div`
+  ${tw`flex justify-between items-center w-full h-full px-2 2xl:px-16 bg-white`}
+`;
