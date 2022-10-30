259ce98ca180b36712ae17658b300404312c5c90
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-10-30 22:58:15 +0000

    moved RainbowTitle styled component and placed it within AboutMe component

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -7,6 +7,7 @@ const AboutMe = () => {
     <div css={tw`w-full h-screen text-center`}>
       <AboutContainer>
         <div>
+          <RainbowTitle>Welcome to PJS-Coder-Portfolio</RainbowTitle>
           <p>
             <q>
               Creativity and technology I believe go hand in hand. It is
@@ -31,4 +32,24 @@ p-2
 flex
 justify-center
 items-center
+`;
+
+const RainbowTitle = styled.h1`
+  background: linear-gradient(
+    to right,
+    #ef5350,
+    #f48fb1,
+    #7e57c2,
+    #2196f3,
+    #26c6da,
+    #43a047,
+    #eeff41,
+    #f9a825,
+    #ff5722
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  margin: 0;
+  line-height: 1.15;
+  font-size: 4rem;
 `;
