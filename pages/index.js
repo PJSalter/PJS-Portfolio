6aeaee5d8b689b432bdb7c6b0fc7f59d7ceb54ea
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-11-01 18:28:04 +0000

    removed code that was not needed and added component into index.js file

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import tw from "twin.macro";
 import Navbar from "../components/NavBar";
 import AboutMe from "../components/AboutMe";
+import FrontPresentedPage from "../components/FrontPresentedPage";
 
 export default function Home() {
   return (
@@ -16,7 +17,7 @@ export default function Home() {
       </Head>
       <Navbar />
       <MainStyle>
-        {/* <RainbowTitle /> */}
+        <FrontPresentedPage />
         <AboutMe />
       </MainStyle>
       <ConditionalButton switchStyle={false}>Press Me</ConditionalButton>
@@ -33,26 +34,6 @@ const MainStyle = styled.main`
   justify-content: center;
   align-items: center;
 `;
-
-// const RainbowTitle = styled.h1`
-//   background: linear-gradient(
-//     to right,
-//     #ef5350,
-//     #f48fb1,
-//     #7e57c2,
-//     #2196f3,
-//     #26c6da,
-//     #43a047,
-//     #eeff41,
-//     #f9a825,
-//     #ff5722
-//   );
-//   -webkit-background-clip: text;
-//   -webkit-text-fill-color: transparent;
-//   margin: 0;
-//   line-height: 1.15;
-//   font-size: 4rem;
-// `;
 
 const ConditionalButton = styled.button(({ switchStyle }) => [
   switchStyle
