291dd931eee2ca7eaf542e9c0477f2f338ed3c37
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-11-04 00:09:56 +0000

    container stylng with tailwind and styled components

--- components/MySkills.jsx
+++ components/MySkills.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import tw from "twin.macro";
+import styled from "styled-components";
 
 const MySkills = () => {
   return (
-    <div css={tw`w-full lg:h-screen p2`}>
+    <div css={tw`w-full lg:h-screen p-2`}>
       <SkillsContainer></SkillsContainer>
     </div>
   );
 };
 
 export default MySkills;
+
+const SkillsContainer = tw.div`
+max-w-[1240px]
+mx-auto
+flex
+flex-col
+justify-center
+h-full
+`;
