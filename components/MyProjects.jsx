28496e4b713c549fc7a6944e2d6180ee50ff2f7a
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-01-12 20:32:01 +0000

    title style component with rainbow affect

--- components/MyProjects.jsx
+++ components/MyProjects.jsx
@@ -6,7 +6,7 @@ const MyProjects = () => {
   return (
     <div className="w-full">
       <ProjectSectionContainer>
-        <p>Projects</p>
+        <MyTitleForProjects>Projects</MyTitleForProjects>
         <h2>What I have built</h2>
         <ContainerCarryingMyBuiltProjects></ContainerCarryingMyBuiltProjects>
       </ProjectSectionContainer>
@@ -21,5 +21,30 @@ const ProjectSectionContainer = styled.div`
 `;
 
 const ContainerCarryingMyBuiltProjects = styled.div`
-  ${tw`max-w-[1240px] mx-auto px-2 py-16`}
+  ${tw`grid md:grid-cols-2 gap-8`}
+`;
+
+// const MyTitleForProjects = styled.div`
+//   ${tw`text-xl tracking-widest uppercase text-[]`}
+// `;
+
+const MyTitleForProjects = styled.p`
+  background: linear-gradient(
+    to right,
+    #ef5350,
+    #f48fb1,
+    #7e57c2,
+    #2196f3,
+    #26c6da,
+    #43a047,
+    #eeff41,
+    #f9a825,
+    #ff5722
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  margin: 0;
+  line-height: 1.15;
+  font-size: 4rem;
+  padding: 5%;
 `;
