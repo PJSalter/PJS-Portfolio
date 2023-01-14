94e1f3936a7734cb6bcffdd79027fb52f7327592
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-01-14 22:46:23 +0000

    created new component for section on each project item

---
+++ components/CreativeProjectItem.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Image from "next/image";
+import styled from "styled-components";
+import tw from "twin.macro";
+import Link from "next/link";
+import techForBetterProject from "../public/assets/Projects/alis-learning-project.png";
+
+const CreativeProjectItem = () => {
+  return (
+    <DevelopmentProject className="group">
+      <Image
+        className="rounded-xl group-hover:opacity-10"
+        src={techForBetterProject}
+        alt="/"
+      />
+      <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <h3 className="text-2xl text-white tracking-wider text-center">
+          Alis Learning
+        </h3>
+        <p className="pb-4 pt-2 text-white text-center">Next.js React</p>
+        <Link href="/">
+          <p className="text-center py-3 rounded-lg bg-white text-yellow-500 font-bold text-lg cursor-pointer">
+            An interactive app that helps dyslexic children read and write
+          </p>
+        </Link>
+      </div>
+    </DevelopmentProject>
+  );
+};
+
+export default CreativeProjectItem;
+
+const DevelopmentProject = styled.div`
+  ${tw`relative flex items-center justify-center h-auto w-full shadow-xl shadow-[#9892B1] rounded-xl p-4 hover:bg-gradient-to-r from-[#5651e5] to-cyan-900`}
+`;
