a10321de45738daceb888056e71e5ceaf89bcc31
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-01-14 21:07:43 +0000

    hover with opacity and button with description of project for user to click link

--- components/MyProjects.jsx
+++ components/MyProjects.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import Image from "next/image";
+import Link from "next/link";
 import techForBetterProject from "../public/assets/Projects/alis-learning-project.png";
 
 const MyProjects = () => {
@@ -11,8 +12,23 @@ const MyProjects = () => {
         <MyTitleForProjects>Projects</MyTitleForProjects>
         <h2 className="py-4">What I have built</h2>
         <ContainerCarryingMyBuiltProjects>
-          <DevelopmentProject>
-            <Image src={techForBetterProject} alt="/" />
+          <DevelopmentProject className="group">
+            <Image
+              className="rounded-xl group-hover:opacity-10"
+              src={techForBetterProject}
+              alt="/"
+            />
+            <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+              <h3 className="text-2xl text-white tracking-wider text-center">
+                Alis Learning
+              </h3>
+              <p className="pb-4 pt-2 text-white text-center">Next.js React</p>
+              <Link href="/">
+                <p className="text-center py-3 rounded-lg bg-white text-yellow-500 font-bold text-lg cursor-pointer">
+                  An interactive app that helps dyslexic children read and write
+                </p>
+              </Link>
+            </div>
           </DevelopmentProject>
         </ContainerCarryingMyBuiltProjects>
       </ProjectSectionContainer>
