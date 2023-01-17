25918bff2d0cbf44a697bb59dbc3e8bbc4aa4fb8
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-01-17 17:18:26 +0000

    description created for each project

--- components/MyProjects.jsx
+++ components/MyProjects.jsx
@@ -23,62 +23,72 @@ const MyProjects = () => {
         <h2 className="py-4">What I have built</h2>
         <ContainerCarryingMyBuiltProjects>
           <CreativeProjectItem
-            title="Tech for better project"
+            title="Tech for better project - Alis Learning"
             backgroundImg={techForBetterProject}
+            Desc="A dyslexic learning app aimed at children aged 5 - 12 to help them improve their reading in a fun engaging way."
             subTitle="Next.js, React, JavaScript, Styled Components and deployed on Netlify"
             projectUrl="https://github.com/fac24/alis-learning"
           />
           <CreativeProjectItem
-            title="Art therapy App"
+            title="Paintbox"
             backgroundImg={paintboxApp}
+            Desc="Art therapy App - users can upload art that reflects their mood. - Users will also be able to upload art inspired by the weekly prompt that can be viewed in a public gallery."
             subTitle="Next.js, React, JavaScript, Cypress for testing, Supabase, Styled Components and deployed on Vercel"
             projectUrl="https://github.com/fac24/paintbox"
           />
           <CreativeProjectItem
             title="Little Garden App"
             backgroundImg={gardenShop}
+            Desc="An e-commerce site for plant-lovers to buy fresh, home-grown plants."
             subTitle="Next.js, React, JavaScript, CSS, PostgreSQL and deployed on Vercel"
             projectUrl="https://github.com/fac24/week6-asmahan-hallie-peter-maria"
           />
           <CreativeProjectItem
             title="Broccoli Game"
             backgroundImg={reactGame}
+            Desc="Broccoli theme game where user has to type github username. User will have to guess and pair the correct hidden username underneath the broccoli's."
             subTitle="React, JavaScript, CSS and deployed on Netlify"
             projectUrl="https://github.com/fac24/week-5-Peter-Hussain"
           />
           <CreativeProjectItem
             title="Rainbow themed tic tac toe game"
             backgroundImg={RainbowTicTacToe}
+            Desc="A strategic battle by two users going head to head, who will win, both users will choose 'O' OR 'X' from one square within a grid layout of nine squares. The user who gets three similar symbols in a row will win the game. If no player wins then game ends with a draw."
             subTitle="Next.js, React, TypeScript, Styled Components and deployed on Netlify"
             projectUrl="https://github.com/PJSalter/pjs-tic-tac-toe"
           />
           <CreativeProjectItem
             title="Binge Pad App"
             backgroundImg={bingePad}
+            Desc="A place for adding lists of all your favourite Binge Worthy Movies."
             subTitle="HTML, CSS, JavaScript"
             projectUrl="https://github.com/AsmahanM/FAC-To-Do-Project"
           />
           <CreativeProjectItem
             title="PJS Pokedex"
             backgroundImg={pokedex}
+            Desc="Pokemon sound effects triggered by click handlers"
             subTitle="HTML, CSS, JavaScript"
             projectUrl="https://github.com/PJSalter/pjs-pokedex-gaming-api"
           />
           <CreativeProjectItem
             title="Sheldonia App"
             backgroundImg={sheldonia}
+            Desc="App that queries two api's and displays as feature's from DOM Manipulation."
             subTitle="HTML, CSS, JavaScript"
             projectUrl="https://github.com/PJSalter/Http-App-Project"
           />
           <CreativeProjectItem
             title="VED Agency"
             backgroundImg={techAgency}
+            Desc="Agency Website named VED"
             subTitle="HTML, CSS, JavaScript"
             projectUrl="https://github.com/sonianb/agency-website"
           />
           <CreativeProjectItem
             title="Animated Dancing Broccoli"
             backgroundImg={broccoliAnimation}
+            Desc="A fun and Interactive dancing Broccoli animation"
             subTitle="HTML, CSS, JavaScript"
             projectUrl="https://github.com/PJSalter/Dancing-Broccoli"
           />
