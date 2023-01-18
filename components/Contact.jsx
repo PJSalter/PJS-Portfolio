e234732aedc9e5e8901c298816a602ec1f41a392
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-01-18 20:35:29 +0000

    styling for contact title

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -4,10 +4,32 @@ const Contact = () => {
   return (
     <div className="w-full lg:h-screen">
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
-        <p></p>
+        <ContactMeTitle>Contact</ContactMeTitle>
+        <h2>Get in Touch</h2>
       </div>
     </div>
   );
 };
 
 export default Contact;
+
+const ContactMeTitle = styled.p`
+  background: linear-gradient(
+    to right,
+    #ef5350,
+    #f48fb1,
+    #7e57c2,
+    #2196f3,
+    #26c6da,
+    #43a047,
+    #eeff41,
+    #f9a825,
+    #ff5722
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  margin: 0;
+  line-height: 1.15;
+  font-size: 4rem;
+  padding: 5%;
+`;
