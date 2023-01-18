31fc4a803fa1b64b245a6de1b626ab71e33cde0b
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-01-18 21:56:07 +0000

    imported twin macro for contact styling of both styled components and tailwind CSS

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styled from "styled-components";
+import tw from "twin.macro";
 
 const Contact = () => {
   return (
     <div className="w-full lg:h-screen">
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
         <ContactMeTitle>Contact</ContactMeTitle>
-        <h2>Get in Touch</h2>
+        <h2 className="py-4">Get in Touch</h2>
+        <GridSystem></GridSystem>
       </div>
     </div>
   );
@@ -33,4 +35,8 @@ const ContactMeTitle = styled.p`
   line-height: 1.15;
   font-size: 4rem;
   padding: 5%;
+`;
+
+const GridSystem = styled.div`
+  ${tw`grid md:grid-cols-2 gap-8`}
 `;
