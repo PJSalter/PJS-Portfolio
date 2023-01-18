475a78e085260a55f69caeca57d33567691d37ea
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-01-18 20:32:14 +0000

    container for contact page with p tag and styling

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="w-full lg:h-screen">
+      <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
+        <p></p>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
