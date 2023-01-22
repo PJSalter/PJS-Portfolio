d46fcb35defb7b075257d76fda7bd0d34e1c5fba
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-01-22 21:24:45 +0000

    commented out path

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import pic from "../public/assets/pjs-software-developer.png";
+// import pic from "../public/assets/pjs-software-developer.png";
 import styled from "styled-components";
 import tw from "twin.macro";
 import Link from "next/link";
