4069bfe551cbc7c8528cd6fa385df417540f6e9e
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-01-24 20:13:31 +0000

    added links to each page in mobile view nav bar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -117,16 +117,16 @@ const Navbar = () => {
               <Link href="/">
                 <li css={tw`py-4 text-sm`}>Home</li>
               </Link>
-              <Link href="/">
+              <Link href="/#about">
                 <li css={tw`py-4 text-sm`}>About</li>
               </Link>
-              <Link href="/">
+              <Link href="/#skills">
                 <li css={tw`py-4 text-sm`}>Skills</li>
               </Link>
-              <Link href="/">
+              <Link href="/#projects">
                 <li css={tw`py-4 text-sm`}>Projects</li>
               </Link>
-              <Link href="/">
+              <Link href="/#contact">
                 <li css={tw`py-4 text-sm`}>Contact</li>
               </Link>
             </ul>
