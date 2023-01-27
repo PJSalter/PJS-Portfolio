1362293710a5525392e77254b11b935c7d161c8f
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-01-27 18:46:56 +0000

    different hex code to color combination of 3d buttons to projects

--- components/CreativeProjectItem.jsx
+++ components/CreativeProjectItem.jsx
@@ -81,8 +81,8 @@ const TextLink = styled.h3`
     color: #382b22;
     text-transform: uppercase;
     padding: 1.25em 2em;
-    background: #fff0f0;
-    border: 2px solid #b18597;
+    background: #f6f0ff;
+    border: 2px solid #9c85b1;
     border-radius: 0.75em;
     transform-style: preserve-3d;
     transition: transform 150ms cubic-bezier(0, 0, 0.58, 1),
@@ -97,7 +97,7 @@ const TextLink = styled.h3`
     left: 0;
     right: 0;
     bottom: 0;
-    background: #f9c4d2;
+    background: #dcc4f9;
     border-radius: inherit;
     box-shadow: 0 0 0 2px #b18597, 0 0.625em 0 0 #87b595;
     transform: translate3d(0, 0.75em, -1em);
@@ -105,19 +105,19 @@ const TextLink = styled.h3`
       box-shadow 150ms cubic-bezier(0, 0, 0.58, 1);
   }
   &.access-project:hover {
-    background: #ffe9e9;
+    background: #f6f0ff;
     transform: translate(0, 0.25em);
   }
   &.access-project:hover::before {
-    box-shadow: 0 0 0 2px #b18597, 0 0.5em 0 0 #ffe3e2;
+    box-shadow: 0 0 0 2px #9c85b1, 0 0.5em 0 0 #f3e2ff;
     transform: translate3d(0, 0.5em, -1em);
   }
   &.access-project:active {
-    background: #ffe9e9;
+    background: #f6f0ff;
     transform: translate(0em, 0.75em);
   }
   &.access-project:active::before {
-    box-shadow: 0 0 0 2px #b18597, 0 0 #ffe3e2;
+    box-shadow: 0 0 0 2px #9c85b1, 0 0 #f3e2ff;
     transform: translate3d(0, 0, -1em);
   }
 
